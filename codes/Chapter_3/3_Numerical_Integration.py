0a887c7fe14a02523a9595d26089268373ac6dc2
bradenmweight <braden.m.weight@gmail.com>
2023-04-28 17:32:10 -0400

    Added intregral boxes

--- codes/Chapter_3/3_Numerical_Integration.py
+++ codes/Chapter_3/3_Numerical_Integration.py
@@ -6,7 +6,18 @@ from scipy import integrate
 
 ################# BEGIN USER INPUT #################
 def get_globals():
-    global DATA_DIR
+    global XMIN, XMAX, DATA_DIR
+
+    """Parabola"""
+    XMIN = 0
+    XMAX = 1
+    """Sine"""
+    #XMIN = 0
+    #XMAX = 1
+    """Gaussian"""
+    #XMIN = -10
+    #XMAX = 10
+
 
     DATA_DIR = "3_PLOTS_DATA"
     sp.call(f"mkdir -p {DATA_DIR}",shell=True)
@@ -16,14 +27,70 @@ def get_f_x( x ):
         'x' can be number or array
         """
         return x ** 2
+        #return np.sin(2 * np.pi * x)
+        #return np.exp(-x**2 / 2)
 
 def get_exact_integral():
     return 1/3
-
+    #return 0.0
+    #return np.sqrt(2 * np.pi)
 ################# END USER INPUT #################
 
-#def get_NUMPY_deriv(f_x):
-#    return np.gradient(f_x,GRID)
+
+def plot_Riem_Trap():
+    Nx_fine     = 1000
+    Nx_coarse   = 10
+    GRID_fine   = np.linspace( XMIN,XMAX,Nx_fine ) # START, END, NPOINTS  
+    GRID_coarse = np.linspace( XMIN,XMAX,Nx_coarse ) # START, END, NPOINTS  
+    f_x_fine    = get_f_x( GRID_fine )
+    f_x_coarse  = get_f_x( GRID_coarse )
+
+    plt.plot( GRID_fine, f_x_fine, "-", c="black", lw=3, label="f(x)" )
+
+    for n in range( Nx_coarse - 1 ):
+         if (n == 0):
+            X = [ GRID_coarse[n],  GRID_coarse[n+1]  ]
+            Y = [ f_x_coarse[n], f_x_coarse[n] ]
+            plt.plot( X, Y, c='red', label="RIEM" )
+            plt.stem( X, Y, linefmt="red", markerfmt="red")
+         else:
+            X = [ GRID_coarse[n],  GRID_coarse[n+1]  ]
+            Y = [ f_x_coarse[n], f_x_coarse[n] ]
+            plt.plot( X, Y, c='red')
+            plt.stem( X, Y, linefmt="red", markerfmt="red")
+
+    plt.legend()
+    plt.xlabel("x",fontsize=15)
+    plt.ylabel("f(x)",fontsize=15)
+    plt.title("Riemann Boxes",fontsize=15)
+    plt.ylim(np.min(f_x_fine), np.max(f_x_fine))
+    plt.savefig(f"{DATA_DIR}/RIEMANN_FIGURE.jpg",dpi=600)
+    plt.clf()
+
+
+    plt.plot( GRID_fine, f_x_fine, "-", c="black", lw=3, label="f(x)" )
+
+    for n in range( Nx_coarse - 1 ):
+         if (n == 0):
+            X = [ GRID_coarse[n],  GRID_coarse[n+1]  ]
+            Y = [ f_x_coarse[n], f_x_coarse[n+1] ]
+            plt.plot( X, Y, c='red', label="TRAP" )
+            plt.stem( X, Y, linefmt="red", markerfmt="red")
+         else:
+            X = [ GRID_coarse[n],  GRID_coarse[n+1]  ]
+            Y = [ f_x_coarse[n], f_x_coarse[n+1] ]
+            plt.plot( X, Y, c='red')
+            plt.stem( X, Y, linefmt="red", markerfmt="red")
+
+    plt.legend()
+    plt.xlabel("x",fontsize=15)
+    plt.ylabel("f(x)",fontsize=15)
+    plt.title("Trapazoidal Boxes",fontsize=15)
+    plt.ylim(np.min(f_x_fine), np.max(f_x_fine))
+    plt.savefig(f"{DATA_DIR}/TRAPAZOIDAL_FIGURE.jpg",dpi=600)
+    plt.clf()
+
+
 
 def get_Riemann_Integral(f_x, GRID):
     # For-loop version
@@ -50,7 +117,7 @@ def get_trapazoidal_Integral(f_x, GRID):
 def test_methods():
     # Let's test all the methods once
     Nx = 100
-    GRID = np.linspace( 0,1,Nx ) # START, END, NPOINTS   
+    GRID = np.linspace( XMIN,XMAX,Nx ) # START, END, NPOINTS   
 
     f_x   = get_f_x( GRID )
     I_R   = get_Riemann_Integral(f_x, GRID)
@@ -80,7 +147,7 @@ def get_convergence_plot():
     SIMP    = np.zeros(( len(Nx_LIST) ))
     QUAD    = np.zeros(( len(Nx_LIST) ))
     for n,N in enumerate( Nx_LIST ):
-            GRID = np.linspace( 0,1,N ) # START, END, NPOINTS 
+            GRID = np.linspace( XMIN,XMAX,N ) # START, END, NPOINTS 
             f_x         = get_f_x( GRID )
             RIEM[n]     = get_Riemann_Integral(f_x, GRID)
             TRAP_MAN[n] = get_trapazoidal_Integral(f_x, GRID)
@@ -104,6 +171,7 @@ def get_convergence_plot():
 
 def main():
     get_globals()
+    plot_Riem_Trap()
     test_methods()
     get_convergence_plot()
 

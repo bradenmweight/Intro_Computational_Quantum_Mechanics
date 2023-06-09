f6cf44f01ae2e3ead997d47264d064be88f7e3d8
bradenmweight <braden.m.weight@gmail.com>
2023-06-09 16:31:59 -0400

    Updated DFT code

--- notes/codes/Chapter_4/1_Fourier_Transform_on_a_Discrete_Grid.py
+++ notes/codes/Chapter_4/1_Fourier_Transform_on_a_Discrete_Grid.py
@@ -12,44 +12,75 @@ def getGlobals():
     DATA_DIR = "1_PLOTS_DATA/"
     sp.call(f"mkdir -p {DATA_DIR}", shell=True)
 
-    global x, dx, Lx, Nx
-    x   = np.linspace(-50,50,1001)
-
-    Nx  = len(x)
-    Lx  = x[-1] - x[0]
-    dx  = x[1]  - x[0]
-
-
 def get_f_x():
-    
+    global x, dx, Lx, Nx
+
     # Gaussian Function
+    x   = np.linspace(-20,20,1001)
     f_x = np.sqrt(1/2/np.pi) * np.exp(-x**2 / 2) + 0j
-    
-    # Sinusoidal function of frequency k = +- 1
+
+    ## Shifted Gaussian Function
+    ## Shift in real-space results is phase change of the DFT
+    ## f(x) --> f(x+dx) => f(k) --> f(k) * e^{-i k dx}
+    ## This is called the "Fourier Shift Theorem"
+    #x     = np.linspace(-20,20,1001)
+    #global shift # Use this to plot analytic result later
+    #shift = 1.0
+    #f_x   = np.sqrt(1/2/np.pi) * np.exp(-(x-shift)**2 / 2) + 0j
+
+    ## Gaussian Function with complex factor (opposite as previous one)
+    ## Shift in real-space results is phase change of the DFT
+    #x     = np.linspace(-20,20,1001)
+    #global p0 # Use this to plot analytic result later
+    #p0 = 1.0 # Initial momentum --> Intepret as a coordinate shift in momentum space
+    #factor = np.exp( -1j * 2 * np.pi * p0 * x )
+    #f_x   = np.sqrt(1/2/np.pi) * np.exp(-x**2 / 2) * factor
+
+    ## Sinusoidal function of frequency k = +- 1
+    ## Large grid sampling
+    #x   = np.linspace(-20,20,1001)
     #f_x = np.sin( 2 * np.pi * x ) + 0j
     
-    # Addition of two waves
+    ## Sinusoidal function of frequency k = +- 1
+    ## Small grid sampling
+    #x   = np.linspace(-2,2,101)
+    #f_x = np.sin( 2 * np.pi * x ) + 0j
+
+    ## Addition of two waves
+    #x   = np.linspace(-10,10,2001)
     #f_x = np.sin( 2 * np.pi * x ) + 0.5 * np.sin( np.pi * x ) + 0j
     
-    # Gaussian-dressed wave-packet
-    #f_x = np.exp(-x**2 / 2 / 1**2) * np.sin( 5 * x ) + 0j
+    ## Gaussian-dressed wave-packet
+    #x   = np.linspace(-10,10,1001)
+    #f_x = np.exp(-x**2 / 2 / 1**2) * np.sin( 2 * np.pi * x ) + 0j
     
-    #f_x = np.ones( len(x) ) # f(x) = 1.0 --> f(k) = delta(x)
+    ##f(x) = 1.0 --> f(k) = delta(k)
+    #x   = np.linspace(-10,10,1001)
+    #f_x = np.ones( len(x) )
     
-    # Single-frequency plane-waves (only positive / only negative)
+    ## Single-frequency plane-waves (only positive / only negative)
+    #x   = np.linspace(-10,10,2001)
     #f_x = np.exp( 1j * 2 * np.pi * x ) # cos + sin
     #f_x = np.exp( -1j * 2 * np.pi * x ) # cos - sin
 
-    # Weird Function -- but which has a frequency
-    #f_x = np.exp( np.sin( np.pi * x ) )
+    ## Weird Function -- but which has a frequency
+    #x   = np.linspace(-50,50,1001)
+    #f_x = np.exp( np.sin( np.pi * x ) ) * np.exp( - x**2 / 2 / 5 )
+
+
+    # Initialize useful numbers
+    Nx  = len(x)
+    Lx  = x[-1] - x[0] # Last minus first
+    dx  = x[1]  - x[0] # Second minus first
 
     return f_x
 
 def plot_f_x(f_x):
-    plt.plot( x, f_x.real, label="RE" )
-    plt.plot( x, f_x.imag, label="IM" )
+    plt.plot( x, f_x.real, c="black", label="RE" )
+    plt.plot( x, f_x.imag, c="red", label="IM" )
     plt.legend()
     plt.xlim(x[0],x[-1])
+    #plt.xlim(-2,2)
     plt.xlabel("x", fontsize=18)
     plt.ylabel("f(x)", fontsize=18)
     plt.tight_layout()
@@ -139,12 +170,20 @@ def plot_f_k(k,f_k,k_np,f_k_np,title):
     plt.clf()
 
 
+    #########################################
     # Plot a ''clean'' one without comparing to Numpy
-    plt.plot( k, f_k.real, "-", c='black', lw=4, label="RE" )
-    plt.plot( k, f_k.imag, "-", c='red',   lw=4, label="IM" )
+    plt.plot( k, f_k.real, "-", c='black', lw=2, label="RE" )
+    plt.plot( k, f_k.imag, "-", c='red',   lw=2, label="IM" )
 
     # Gaussian function in k-space -- Analytic Result
-    #plt.plot( k, np.sqrt(1/2/np.pi) * np.exp(-2 * k**2 * np.pi**2), c="green", label="Gaussian" )
+    #analytic_func_k = np.sqrt(1/2/np.pi) * np.exp(-2 * k**2 * np.pi**2)
+    #plt.plot( k, analytic_func_k, c="green", label="Gaussian" )
+
+    # Shifted Gaussian function
+    #analytic_func_k = np.sqrt(1/2/np.pi) * np.exp(-2 * k**2 * np.pi**2)
+    #shifted_analytic = np.exp(-1j * 2 * np.pi * k * shift)  * analytic_func_k
+    #plt.plot( k, shifted_analytic.real, "o", c="black", label="Analytic RE" )
+    #plt.plot( k, shifted_analytic.imag, "o", c="red", label="Analytic IM" )
 
     plt.legend()
     plt.xlim(-2,2 )

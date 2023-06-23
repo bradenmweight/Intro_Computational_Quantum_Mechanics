2c3b28d82cbade4b22b199cc8a41616e097253c9
bradenmweight <braden.m.weight@gmail.com>
2023-06-23 16:11:18 -0400

    Improved plotting

--- homeworks/solutions/Chapter_4/4.1.1_Expectation_Values.py
+++ homeworks/solutions/Chapter_4/4.1.1_Expectation_Values.py
@@ -22,16 +22,16 @@ def plot_f_x(x,f_x):
     color_list = ['black','red','green','blue','orange','purple']
     for p0IND,p0 in enumerate( p0_LIST[:2] ):
         if ( p0IND == 0 ):
-            plt.plot( x, f_x[p0IND].real, "--", c=color_list[p0IND], label=f"p0 = {round(p0,1)} RE" )
-            plt.plot( x, f_x[p0IND].imag, ":", c=color_list[p0IND], label=f"p0 = {round(p0,1)} IM" )
-            plt.plot( x, np.abs(f_x[p0IND]), "-", lw=1, c=color_list[p0IND], label=f"p0 = {round(p0,1)} ABS" )
+            plt.plot( x, f_x[p0IND].real, "--", lw=3, c=color_list[p0IND], label=f"p0 = {round(p0,1)} RE" )
+            plt.plot( x, f_x[p0IND].imag, ":", lw=3, c=color_list[p0IND], label=f"p0 = {round(p0,1)} IM" )
+            plt.plot( x, np.abs(f_x[p0IND]), "-", lw=3, c=color_list[p0IND], label=f"p0 = {round(p0,1)} ABS" )
         else:
-            plt.plot( x, f_x[p0IND].real, "--", c=color_list[p0IND], label=f"p0 = {round(p0,1)}" )
-            plt.plot( x, f_x[p0IND].imag, ":", c=color_list[p0IND] )
-            plt.plot( x, np.abs(f_x[p0IND]), "-", lw=1, c=color_list[p0IND] )
+            plt.plot( x, f_x[p0IND].real, "--", lw=2, c=color_list[p0IND] )
+            plt.plot( x, f_x[p0IND].imag, ":", lw=2, c=color_list[p0IND] )
+            plt.plot( x, np.abs(f_x[p0IND]), "-", lw=1, c=color_list[p0IND], label=f"p0 = {round(p0,1)}" )
 
     plt.legend()
-    plt.xlim(-5,5)
+    plt.xlim(-4,4)
     plt.xlabel("x",fontsize=15)
     plt.ylabel("Wavefunction",fontsize=15)
     plt.savefig(f"{DATA_DIR}/f_x.jpg",dpi=400)
@@ -97,9 +97,9 @@ def get_observables( x,f_x,p,f_p ):
     dx = x[1] - x[0]
     dp = p[1] - p[0]
     
-    # Define the conjugate of each for all p (implicit)
-    f_x_conj = np.conjugate( f_x )
-    f_p_conj = np.conjugate( f_p )
+    # Define the conjugate of each for each p (implicit)
+    f_x_conj = np.conjugate( f_x[:,:] )
+    f_p_conj = np.conjugate( f_p[:,:] )
 
     # Define observables
     X_AVE  = np.zeros( (len(p0_LIST)) )
@@ -124,20 +124,32 @@ def get_observables( x,f_x,p,f_p ):
 
 def plot_OBSERVABLES( X_AVE, X2_AVE, P_AVE, P2_AVE ):
 
-    plt.plot( p0_LIST, X_AVE,"-o", label="<X>" )
-    plt.plot( p0_LIST, X2_AVE,"-o", label="<X^2>" )
+    plt.plot( p0_LIST, X_AVE,"-o", label="$\langle X \\rangle$" )
+    plt.plot( p0_LIST, X2_AVE,"-o", label="$\langle X^2 \\rangle$" )
     plt.legend()
     plt.savefig(f"{DATA_DIR}/X_X2.jpg",dpi=400)
     plt.clf()
 
-    plt.plot( p0_LIST, P_AVE,"-o", label="<P>" )
+    plt.plot( p0_LIST, np.round(X2_AVE - X_AVE**2,5),"-o", label="$\langle (\Delta X)^2 \\rangle$" )
+    plt.legend()
+    plt.title("Constant $\langle (\Delta X)^2 \\rangle$ implies that the width of f(x) is constant.",fontsize=15)
+    plt.savefig(f"{DATA_DIR}/X_VAR.jpg",dpi=400)
+    plt.clf()
+
+    plt.plot( p0_LIST, P_AVE,"-o", label="$\langle P \\rangle$" )
     plt.legend()
     plt.savefig(f"{DATA_DIR}/P.jpg",dpi=400)
     plt.clf()
 
-    plt.plot( p0_LIST, P2_AVE,"-o", label="<P^2>" )
+    plt.plot( p0_LIST, P2_AVE,"-o", label="$\langle P^2 \\rangle$" )
     plt.legend()
     plt.savefig(f"{DATA_DIR}/P2.jpg",dpi=400)
+    plt.clf()
+
+    plt.plot( p0_LIST, np.round(P2_AVE - P_AVE**2,5),"-o", label="$\langle (\Delta P)^2 \\rangle$" )
+    plt.legend()
+    plt.title("Constant $\langle (\Delta P)^2 \\rangle$ implies that the width of f(p) is constant.",fontsize=15)
+    plt.savefig(f"{DATA_DIR}/P_VAR.jpg",dpi=400)
     plt.clf()
 
 
